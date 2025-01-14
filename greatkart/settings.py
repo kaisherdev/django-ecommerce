4a8bc16754217bc275d4e021a85a22f9a4769a2e
Wilson Dev <hch.wilson@hynkor.com>
2025-01-14 11:59:20 -0500

    ✨ feat(greatkart): add media files configurations

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -125,6 +125,11 @@ STATICFILES_DIRS = [
     'greatkart/static',
 ]
 
+# Media files configurations
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
