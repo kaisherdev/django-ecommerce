100b281599b5077678ab854e7784507f90af592f
Wilson Dev <hch.wilson@hynkor.com>
2025-01-15 20:19:37 -0500

    ✨ feat(store): single product view

--- store/views.py
+++ store/views.py
@@ -22,4 +22,11 @@ def store(request,category_slug=None):
     return render(request,'store/store.html', contex)
 
 def product_detail(request,category_slug,product_slug):
-    return render(request,'store/product_detail.html')
+    try:
+        single_product = Product.objects.get(category__slug=category_slug,slug=product_slug)
+    except Exception as e:
+        raise e
+    contex = {
+        'single_product':single_product,
+    }
+    return render(request,'store/product_detail.html',contex)
