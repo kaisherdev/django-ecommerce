25f8ad90eb26f192c3dd5da68cce1b453a2574fd
Wilson Dev <hch.wilson@hynkor.com>
2025-01-16 14:15:04 -0500

    ✨ feat(carts): add logic to cart

--- carts/views.py
+++ carts/views.py
@@ -1,6 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from store.models import Product
+from .models import Cart, CartItem
 
 # Create your views here.
+from django.http import HttpResponse
 
+# Create functin _cart_id
+def _cart_id(request):
+    cart = request.session.session_key
+    if not cart:
+        cart = request.session.create()
+    return cart
+
+# Create function add_cart
+def add_cart(request,product_id):
+    # Get the product
+    product = Product.objects.get(id=product_id)
+    try:
+        # Get the cart using the cart_id present in the session.
+        cart = Cart.objects.get(cart_id = _cart_id(request))
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(
+            cart_id = _cart_id(request)
+        )
+    cart.save()
+    try:
+        # Get the cart_item
+        cart_item = CartItem.objects.get(product=product,cart=cart)
+        cart_item.quantity += 1
+        cart_item.save()
+    except CartItem.DoesNotExist:
+        cart_item = CartItem.objects.create(
+            product=product,
+            quantity = 1,
+            cart=cart,
+        )
+        cart_item.save()
+    return HttpResponse(cart_item.cart)
+    exit()
+    return redirect('cart')
+    
 def cart(request):
     return render(request,'store/cart.html')
