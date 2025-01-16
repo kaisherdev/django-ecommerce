6e27f3e4b6876e32b9bf0e707ff1409222a97c58
Wilson Dev <hch.wilson@hynkor.com>
2025-01-16 14:12:31 -0500

    🦄 refactor(carts): define date_added auto_now_add = true

--- carts/models.py
+++ carts/models.py
@@ -5,7 +5,7 @@ from store.models import Product
 
 class Cart(models.Model):
     cart_id = models.CharField(max_length=250,blank=True)
-    date_added = models.DateField()
+    date_added = models.DateField(auto_now_add=True)
     
     def __str__(self):
         return self.cart_id
