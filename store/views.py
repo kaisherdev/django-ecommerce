04db09745d2d39dce8a68ac787644fcaa847e919
Wilson Dev <hch.wilson@hynkor.com>
2025-01-17 16:20:01 -0500

    ✨ feat(store): add functionality paginator part2

--- store/views.py
+++ store/views.py
@@ -3,7 +3,7 @@ from .models import Product
 from category.models import Category
 from carts.models import CartItem
 from carts.views import _cart_id
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 
 # Create your views here.
 
@@ -14,10 +14,13 @@ def store(request,category_slug=None):
     if category_slug != None:
         categories = get_object_or_404(Category,slug=category_slug)
         products = Product.objects.filter(category=categories,is_available=True)
+        paginator = Paginator(products,3) # Paginator
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
     else:
         products = Product.objects.all().filter(is_available=True)
-        paginator = Paginator(products,6) # Paginator
+        paginator = Paginator(products,3) # Paginator
         page = request.GET.get('page')
         paged_products = paginator.get_page(page)
         product_count = products.count()
