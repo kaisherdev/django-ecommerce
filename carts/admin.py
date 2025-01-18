c27c154ede22e6f558c87d96b5325cf4437a08f2
Wilson Dev <hch.wilson@hynkor.com>
2025-01-18 16:19:51 -0500

    ✨ feat(carts): display title table cart and cartitem into panel admin

--- carts/admin.py
+++ carts/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from .models import Cart, CartItem
 # Register your models here.
 
-admin.site.register(Cart)
-admin.site.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('cart','product','quantity','is_active')
+    
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('cart_id','date_added')
+
+admin.site.register(Cart,CartAdmin)
+admin.site.register(CartItem,CartItemAdmin)
