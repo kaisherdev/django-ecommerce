0c3642f8fc8b89c53eac4f976703392591f1e2e9
Wilson Dev <hch.wilson@hynkor.com>
2025-01-17 19:34:37 -0500

    ✨ feat(store): Add search functionality

--- store/views.py
+++ store/views.py
@@ -4,6 +4,7 @@ from category.models import Category
 from carts.models import CartItem
 from carts.views import _cart_id
 from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
+from django.db.models import Q
 
 # Create your views here.
 
@@ -42,3 +43,16 @@ def product_detail(request,category_slug,product_slug):
         'in_cart':in_cart,
     }
     return render(request,'store/product_detail.html',contex)
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by("-created_date").filter(Q(dscription__icontains=keyword) | Q(product_name__icontains=keyword))
+            product_count = products.count()
+        contex = {
+            'products':products,
+            'product_count':product_count,
+            'keyword':keyword,
+        }
+    return render(request,'store/store.html',contex)
