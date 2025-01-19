da05465c987be446fee392b0921e81d0968615f1
Wilson Dev <hch.wilson@hynkor.com>
2025-01-19 14:09:27 -0500

    feat cart increment, decrement, remove button functionality

--- carts/views.py
+++ carts/views.py
@@ -57,8 +57,7 @@ def add_cart(request,product_id):
             if len(product_variation) > 0:
                 item.variations.clear()
                 item.variations.add(*product_variation)
-            # cart_item.quantity += 1
-            item.save()
+                item.save()
     else:
         cart_item = CartItem.objects.create(
             product=product,
@@ -66,26 +65,29 @@ def add_cart(request,product_id):
             cart=cart,
         )
         if len(product_variation) > 0:
-            item.variations.clear()
-            item.variations.add(*product_variation)
-        cart_item.save()
+            cart_item.variations.clear()
+            cart_item.variations.add(*product_variation)
+            cart_item.save()
     return redirect('cart')
 
-def remove_cart(request,product_id):
+def remove_cart(request,product_id,cart_item_id):
     cart = Cart.objects.get(cart_id=_cart_id(request))
     product = get_object_or_404(Product,id=product_id)
-    cart_item = CartItem.objects.get(product=product,cart=cart)
-    if cart_item.quantity > 1:
-        cart_item.quantity -= 1
-        cart_item.save()
-    else:
-        cart_item.delete()
+    try:
+        cart_item = CartItem.objects.get(product=product,cart=cart,id=cart_item_id)
+        if cart_item.quantity > 1:
+            cart_item.quantity -= 1
+            cart_item.save()
+        else:
+            cart_item.delete()
+    except:
+        pass
     return redirect('cart')
 
-def remove_cart_item(request,product_id):
+def remove_cart_item(request,product_id,cart_item_id):
     cart = Cart.objects.get(cart_id=_cart_id(request))
     product = get_object_or_404(Product,id=product_id)
-    cart_item = CartItem.objects.get(product=product,cart=cart)
+    cart_item = CartItem.objects.get(product=product,cart=cart,id=cart_item_id)
     cart_item.delete()
     return redirect('cart')
     
