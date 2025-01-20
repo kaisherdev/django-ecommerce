a2e1459a63cfc3c7ea0643da381474a99a11513e
Wilson Dev <hch.wilson@hynkor.com>
2025-01-19 22:33:36 -0500

    ✨ feat(accounts): create views register, login and logout

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .forms import RegistrationForm
 
 # Create your views here.
+
+def register(request):
+    form = RegistrationForm()
+    contex = {
+        'form':form,
+    }
+    return render(request,'accounts/register.html',contex)
+
+def login(request):
+    return render(request,'accounts/login.html')
+
+def logout(request):
+    pass
+    # return render(request,'accounts/logout.html')s
