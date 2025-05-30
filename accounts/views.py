070a94078005ef25fbe47d02864ec3ee92a2835d
Wilson Dev <hch.wilson@hynkor.com>
2025-05-30 17:27:16 -0500

    ✨ feat: change password

--- accounts/views.py
+++ accounts/views.py
@@ -217,7 +217,8 @@ def resetPassword(request):
             return redirect('resetPassword')
     else:
         return render(request, 'accounts/resetPassword.html')
-    
+
+@login_required(login_url='login')   
 def my_orders(request):
     orders = Order.objects.filter(user=request.user,is_ordered=True).order_by('-created_at')
     context = {
@@ -225,6 +226,7 @@ def my_orders(request):
     }
     return render(request, 'accounts/my_orders.html',context)
 
+@login_required(login_url='login')
 def edit_profile(request):
     userprofile = get_object_or_404(UserProfile,user=request.user)
     if request.method == 'POST':
@@ -245,3 +247,28 @@ def edit_profile(request):
     }
         
     return render(request,'accounts/edit_profile.html',context)
+
+@login_required(login_url='login')
+def change_password(request):
+    if request.method == 'POST':
+        current_password = request.POST['current_password']
+        new_password = request.POST['new_password']
+        confirm_password = request.POST['confirm_password']
+        
+        user = Account.objects.get(username__exact=request.user.username)
+        
+        if new_password == confirm_password:
+            success = user.check_password(current_password)
+            if success:
+                user.set_password(new_password)
+                user.save()
+                # auth.logout(request)
+                messages.success(request,'Password updated successfully.')
+                return redirect('change_password')
+            else:
+                messages.error(request,'Please enter valid correct password')
+                return redirect('change_password')
+        else:
+            messages.error(request,'Password does not match!')
+            return redirect('change_password')
+    return render(request,'accounts/change_password.html')
